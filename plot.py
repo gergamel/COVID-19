42fd1fb097b92ea8b301a9f584cc3b6a355f1f35
Greg.Cook <greg.cook@avi.com.au>
2020-03-15 12:39:03 +0800

    Finished refactor and clean up

--- plot.py
+++ plot.py
@@ -2,20 +2,13 @@
 import sqlite3
 DB_FILE="csse_covid_19.db"
 
-'''
-Province/States,Country/Region,              WHO region,1/21/20,1/22/20,1/23/20,1/24/20,1/25/20,1/26/20,1/27/20,1/28/20,1/29/20,1/30/20,1/31/20,2/1/20,2/2/20,2/3/20,2/4/20,2/5/20,2/6/20,2/7/20,2/8/20,2/9/20,2/10/20,2/11/20,2/12/20,2/13/20,2/14/20,2/15/20,2/16/20,2/17/20,2/18/20,2/19/20,2/20/20,2/21/20,2/22/20,2/23/20,2/24/20,2/25/20,2/26/20,2/27/20,2/28/20,2/29/20,3/1/20,3/2/20,3/3/20
-Confirmed,      Globally,                              ,282,314,581,846,1320,2014,2798,4593,6065,7818,9826,11953,14557,17391,20630,24554,28276,31481,34886,37558,40554,43103,45171,46997,49053,50580,51857,71429,73332,75204,75748,76769,77794,78811,79331,80239,81109,82294,83652,85403,87137,88948,90870
-Confirmed,      Mainland China,  Western Pacific Region,278,309,571,830,1297,1985,2741,4537,5997,7736,9720,11821,14411,17238,20471,24363,28060,31211,34598,37251,40235,42708,44730,46550,48548,50054,51174,70635,72528,74280,74675,75569,76392,77042,77262,77780,78191,78630,78961,79394,79968,80174,80304
-Confirmed,      Outside of China,                      ,4,5,10,16,23,29,57,56,68,82,106,132,146,153,159,191,216,270,288,307,319,395,441,447,505,526,683,794,804,924,1073,1200,1402,1769,2069,2459,2918,3664,4691,6009,7169,8774,10566
-Suspected,      Mainland China,  Western Pacific Region,,,,,,,5794,6973,9239,12167,15238,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
-Severe,         Mainland China,  Western Pacific Region,,,,,,,461,976,1239,1370,1527,1795,2110,2296,2788,3219,3859,4821,6101,6188,6484,7333,8204,,,,,,,,,,,,,,,,,,,,
-Deaths,         Mainland China,  Western Pacific Region,,,,,,,80,106,132,170,213,259,304,361,425,491,564,637,723,812,909,1017,1114,1260,1381,1524,1666,1772,1870,2006,2121,2239,2348,2445,2595,2666,2718,2747,2791,2838,2873,2915,2946
-Hubei ,         China,           Western Pacific Region,258,270,375,375,,,,,,,,7153,9074,11177,13522,16678,19665,22112,24953,27100,29631,31728,33366,34874,51968,54406,56249,58182,59989,61682,62031,62662,63454,64084,64287,64786,65187,65596,65914,66337,66907,67103,67217
-'''
-
 import math
 import datetime as dt
 import numpy as np
+import csv
+from collections import namedtuple
+
+#db_file=':memory:'
 
 def todate(str_date):
     return dt.datetime.strptime(str_date,'%m/%d/%y')
@@ -27,97 +20,112 @@ def toint(str_int):
         pass
     return None
 
-def import_data():
-    import csv
-    #db_file=':memory:'
-    conn = sqlite3.connect(DB_FILE)
-    c = conn.cursor()
-    c.execute('''DROP TABLE IF EXISTS data;''')
-    c.execute('''DROP TABLE IF EXISTS location;''')
-    conn.commit()
-    c.execute('''CREATE TABLE IF NOT EXISTS location (id INTEGER NOT NULL PRIMARY KEY, state text, region text, lat real, long real);''')
-    c.execute('''CREATE TABLE IF NOT EXISTS data (location_id, date integer, cases integer, deaths integer);''')
+Location=namedtuple("location", ('id','state','region','lat','long'))
+Data=namedtuple("data", ('id','location_id', 'date', 'cases'))
+TimeSeries=namedtuple("Series",('t','y'))
+NamedTimeSeries=namedtuple("NamedData",('name','t','y'))
 
-    conf_csv="csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
-    f=open(conf_csv)
-    r=csv.reader(f, delimiter=',')
-    """
-    Headers in the CSV are:
-    0-Province/State
-    1-Country/Region
-    2-Lat
-    3-Long
-    4-1/22/20
-    5-1/23/20
-    etc...
-    """
-    date_row=list(map(todate,r.__next__()[4:]))
-    for line in r:
-        c.execute('INSERT INTO location (state, region, lat, long) VALUES (?,?,?,?)',line[0:4])
-        #c.execute('INSERT INTO location (state, region, lat, long) VALUES (?,?,?,?)',line[0:4])
-        loc_id=c.lastrowid
-        n=0
-        for v in map(toint,line[4:]):
-            c.execute('INSERT INTO data (location_id, date, cases) VALUES (?,?,?)',(loc_id,int(date_row[n].timestamp()),v))
-            n+=1
-    f.close()
-    conn.commit()
-    conn.close()
+def to_location(row):
+     return Location(id=row[0],state=row[1],region=row[2],lat=row[3],long=row[4])
 
-import_data()
+def to_data(row):
+     return Data(id=row[0],location_id=row[1],date=dt.datetime.fromtimestamp(row[2]),lat=row[3],long=row[4])
+
+def to_time_series(row):
+     return TimeSeries(t=dt.datetime.fromtimestamp(row[0]),y=row[1])
+
+class Session():
+    def __init__(self):
+        self.conn = sqlite3.connect(DB_FILE)
+        self.c = self.conn.cursor()
+    def flush(self):
+        self.c.execute('''DROP TABLE IF EXISTS data;''')
+        self.c.execute('''DROP TABLE IF EXISTS location;''')
+        self.conn.commit()
+        self.c.execute('''CREATE TABLE IF NOT EXISTS location (id INTEGER NOT NULL PRIMARY KEY, state text, region text, lat real, long real);''')
+        self.c.execute('''CREATE TABLE IF NOT EXISTS data (location_id, date integer, cases integer, deaths integer);''')
+    def import_data(self):
+        conf_csv="csse_covid_19_data/csse_covid_19_time_series/time_series_19-covid-Confirmed.csv"
+        f=open(conf_csv)
+        r=csv.reader(f, delimiter=',')
+        """
+        Headers in the CSV are:
+        0-Province/State
+        1-Country/Region
+        2-Lat
+        3-Long
+        4-1/22/20
+        5-1/23/20
+        etc...
+        """
+        date_row=list(map(todate,r.__next__()[4:]))
+        for line in r:
+            self.c.execute('INSERT INTO location (state, region, lat, long) VALUES (?,?,?,?)',line[0:4])
+            #c.execute('INSERT INTO location (state, region, lat, long) VALUES (?,?,?,?)',line[0:4])
+            loc_id=self.c.lastrowid
+            n=0
+            for v in map(toint,line[4:]):
+                self.c.execute('INSERT INTO data (location_id, date, cases) VALUES (?,?,?)',(loc_id,int(date_row[n].timestamp()),v))
+                n+=1
+        f.close()
+        self.conn.commit()
+    def execute(self,sql,**kwargs):
+        self.c.execute(sql,**kwargs)
+    def commit(self):
+        self.conn.commit()
+    def close(self):
+        self.conn.close()
+    def get_locations(self):
+        return map(to_location,self.c.execute("SELECT * FROM location ORDER BY region,state;"))
+    def get_regions(self):
+        return self.c.execute("SELECT DISTINCT region FROM location ORDER BY region;")
+    def get_region_totals(self):
+        return self.c.execute("""SELECT l.region, MAX(d.cases) AS cases
+        FROM data AS d JOIN location AS l ON d.location_id=l.id
+        GROUP BY l.region ORDER BY cases;""")
+    def get_series(self,state,x_offset=dt.datetime(2020, 2, 20)):
+        offset_ts=int(x_offset.timestamp()/86400)
+        sql_str=f"""SELECT d.date, d.cases
+        FROM data AS d JOIN location AS l ON d.location_id=l.id
+        WHERE l.state='{state}'
+        GROUP BY d.date;"""
+        results=self.c.execute(sql_str)
+        dates,cases=zip(*results)
+        t=np.floor((np.array(dates, dtype=np.float)/86400) - offset_ts)
+        y=np.array(cases, dtype=np.float)
+        return NamedTimeSeries(name=state,t=t,y=y)
+    def get_region_total_series(self,region,x_offset=dt.datetime(2020, 2, 20)):
+        offset_ts=int(x_offset.timestamp()/86400)
+        sql_str=f"""SELECT d.date, SUM(d.cases) AS cases
+        FROM data AS d JOIN location AS l ON d.location_id=l.id
+        WHERE l.region='{region}'
+        GROUP BY d.date;"""
+        results=self.c.execute(sql_str)
+        dates,cases=zip(*results)
+        t=np.floor((np.array(dates, dtype=np.float)/86400) - offset_ts)
+        y=np.array(cases, dtype=np.float)
+        return NamedTimeSeries(name=region,t=t,y=y)
+
+c=Session()
+c.flush()
+c.import_data()
+c.close()
 
 import matplotlib.pyplot as plt
 import matplotlib.dates as mdates
-from collections import namedtuple
 
-#c.execute('''CREATE TABLE IF NOT EXISTS location (id INTEGER NOT NULL PRIMARY KEY, state text, region text, lat real, long real);''')
-#c.execute('''CREATE TABLE IF NOT EXISTS data (location_id, date integer, cases integer, deaths integer);''')
-
-Location=namedtuple("location", ('id','state','region','lat','long'))
-Data=namedtuple("data", ('id','location_id', 'date', 'cases'))
-RegionData=namedtuple("RegionData",('date','cases'))
-
-def row2location(row):
-     return Location(id=row[0],state=row[1],region=row[2],lat=row[3],long=row[4])
-
-def row2data(row):
-     return Data(id=row[0],location_id=row[1],date=dt.datetime.fromtimestamp(row[2]),lat=row[3],long=row[4])
-
-def row2regiondata(row):
-     return RegionData(date=dt.datetime.fromtimestamp(row[0]),cases=row[1])
-
-def get_locations(c):
-    return map(row2region,c.execute("SELECT * FROM location;"))
-
-def get_regions(c):
-    return c.execute("SELECT DISTINCT region FROM location ORDER BY region;")
-
-conn = sqlite3.connect(DB_FILE)
-c = conn.cursor()
-for r in get_regions(c):
-    print(r)
-
-region_data=list(map(row2regiondata,c.execute("""
-SELECT d.date, SUM(d.cases) AS cases
-FROM data AS d JOIN location AS l ON d.location_id=l.id
-WHERE l.region='Australia'
-GROUP BY d.date""")))
-x0=dt.datetime(2020, 1, 22)
-
-x=tuple(((r.date-x0).days for r in region_data))
-y=tuple((r.cases for r in region_data))
-print(x)
-print(y)
-c.close()
+##########################################################################################
+# MATPLOTLIB HELPER FUNCTIONS
+##########################################################################################
 
 def setup_xaxis(axis,label,limits,tick_spacing,color='black'):
     axis.set_xlabel(label,color=color)
-    axis.set_xticks(np.arange(limits[0],limits[1]+dt.timedelta(seconds=1), tick_spacing[0]))
-    axis.set_xticks(np.arange(limits[0],limits[1]+dt.timedelta(seconds=1), tick_spacing[1]), minor=True)
+    axis.set_xticks(np.arange(limits[0],limits[1]+0.01, tick_spacing[0]))
+    axis.set_xticks(np.arange(limits[0],limits[1]+0.01, tick_spacing[1]), minor=True)
     axis.tick_params(axis='y',which='both',labelcolor=color)
     axis.grid(axis='x',which='major',visible=True,color=color,alpha=0.5,linestyle=':')
     axis.grid(axis='x',which='minor',visible=True,color=color,alpha=0.25,linestyle=':')
-    axis.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b'))
+    #axis.xaxis.set_major_formatter(mdates.DateFormatter('%d-%b'))
     axis.set_xlim(limits)
 
 def setup_yaxis(axis,label,limits,tick_spacing,color='black'):
@@ -138,18 +146,58 @@ def setup_logyaxis(axis,label,limits,tick_spacing,color='black'):
     axis.grid(axis='y',which='minor',visible=True,color=color,alpha=0.25,linestyle=':')
     axis.set_ylim(limits)
 
-xlims=[min(x),max(x)]
+##########################################################################################
+# DATA MODEL
+##########################################################################################
 
-fig, ax = plt.subplots(1,1)
+#c.execute('''CREATE TABLE IF NOT EXISTS location (id INTEGER NOT NULL PRIMARY KEY, state text, region text, lat real, long real);''')
+#c.execute('''CREATE TABLE IF NOT EXISTS data (location_id, date integer, cases integer, deaths integer);''')
+
+##########################################################################################
+
+#conn = sqlite3.connect(DB_FILE)
+#c = conn.cursor()
+c=Session()
+for r in c.get_region_totals():
+    print(r)
+
+plot_regions=(
+'Japan',
+'Singapore',
+'Korea, South',
+'France',
+'Spain',
+'United Kingdom',
+'Italy',
+'US',
+'Australia',
+)
+
+#fig, ax = plt.subplots(1,1)
+ax = plt.gca()
 lines=[]
-ln,=ax[0].plot(x,y)
-ln.set_label(f'Confirmed ({cname})')
-lines.append(ln)
-####
-setup_xaxis(ax[0],"Date",xlims,[dt.timedelta(days=10),dt.timedelta(days=1)],color='black')
-setup_yaxis(ax[0],"Growth (%)",[0,100],[10,2],color='black')
-ax[0].legend(lines, [l.get_label() for l in lines])
 
+data = []
+data.append(c.get_series('Hong Kong'))
+for r in plot_regions:
+    data.append(c.get_region_total_series(r))
+
+c.close()
+tmax=0
+ymax=0
+for s in data:
+    stmax=np.nanmax(s.t)
+    if stmax>tmax: tmax=stmax
+    symax=10000*np.ceil(np.nanmax(s.y)/10000)
+    if symax>ymax: ymax=symax
+    ln,=ax.semilogy(s.t,s.y)
+    ln.set_label(f'{s.name}')
+    lines.append(ln)
+
+setup_xaxis(ax,"Days Since 20-Feb",[0,tmax],[10,1],color='black')
+setup_logyaxis(ax,"Confirmed Cases",[100,ymax],[ymax/10,ymax/20],color='black')
+ax.legend(lines, [l.get_label() for l in lines])
+plt.title("John-Hopkins CSSE COVID-19 Data")
 plt.show()
 
 exit(0)
